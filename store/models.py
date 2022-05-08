6fb263d4e48374d2716234dc4f53da9b7d62eecf
franckalainai <franckalain.ai@gmail.com>
2022-05-08 23:45:40 +0000

    working with products

---
+++ store/models.py
@@ -0,0 +1,21 @@
+from distutils.command.upload import upload
+from tkinter import CASCADE
+from django.db import models
+from django.forms import IntegerField
+from category.models import Category
+
+# Create your models here.
+class Product(models.Model):
+    product_name    = models.CharField(max_length=200, unique=True)
+    slug            = models.SlugField(max_length=200, unique=True)
+    desciption      = models.TextField(max_length=200, blank=True)
+    price           = models.IntegerField()
+    images          = models.ImageField(upload_to = 'photos/products')
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default=True)
+    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date    = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
